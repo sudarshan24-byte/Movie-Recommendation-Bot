58fd909cda607c06dc09bafbb6a1223319545898
Sudcsk24 <rushitrifaley@gmail.com>
2024-09-22 11:52:23 +0530

    initial commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+# Popcorn Picker
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from utils import movie, start, help_command, echo, unknown_command, genre
+
+
+def main():
+    # Replace 'YOUR_TOKEN_HERE' with your actual bot token
+    updater = Updater("7559265658:AAHEik1Mxii6O-qPUpEqs7mca9IiK8-RCLQ", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Register command handlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("movie", movie))
+    dp.add_handler(CommandHandler("genre", genre))
+
+    # Register a message handler to echo all messages
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(Filters.command, unknown_command))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl+C
+    updater.idle()
+
+if __name__ == '__main__':
+    print('Starting...')
+    main()
+    # print('Started...')
---
+++ utils.py
@@ -0,0 +1,41 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+# Start Command
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hello! I am your simple Telegram bot. Send me a message and I will echo it back!')
+
+
+# Define the help command handler
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('I can recommend you movies. Just type /movie and your genre and I will recommend it for you.')
+
+
+# Define the echo function to reply with the same message
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+# For Unknown Command
+def unknown_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f"Sorry, I don't understand that command: {update.message.text}")
+
+
+def movie(update: Update, context: CallbackContext) -> None:
+    # Join the arguments passed after /movie to form the movie name
+    movie_name = ' '.join(context.args)
+
+    if movie_name:  # If user provided a movie name
+        # Reply with the captured movie name
+        update.message.reply_text(f'Your favorite movie is: {movie_name}')
+    else:
+        # If no movie name is provided
+        update.message.reply_text('Please provide a movie name after /movie.')
+
+
+def genre(update: Update, context: CallbackContext) -> None:
+    genre_name = ' '.join(context.args)
+
+    if genre_name:
+        update.message.reply_text(f'Your favorite genre is: {genre_name}')
+    else:
+        update.message.reply_text('Please provide a genre name after /genre.')
